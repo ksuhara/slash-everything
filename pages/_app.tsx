49d1eb75d4a93a0af282801a4f2230edbabc2647
kenta suhara <ponta.blockchain@gmail.com>
2023-02-13 00:41:49 +0900

    auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ThirdwebProvider>
   );
 }
---
+++ pages/api/auth/login.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import initializeFirebaseServer from "../../../configs/initFirebaseAdmin";
+
+export default async function login(req: NextApiRequest, res: NextApiResponse) {
+  // Grab the login payload the user sent us with their request.
+  const loginPayload = req.body.payload;
+  // Set this to your domain to prevent signature malleability attacks.
+  const domain = "slash-everything.vercel.app";
+
+  const sdk = ThirdwebSDK.fromPrivateKey(
+    // https://portal.thirdweb.com/sdk/set-up-the-sdk/securing-your-private-key
+    process.env.ADMIN_PRIVATE_KEY!,
+    "goerli" // configure this to your network
+  );
+
+  let address;
+  try {
+    // Verify the address of the logged in client-side wallet by validating the provided client-side login request.
+    address = sdk.auth.verify(domain, loginPayload);
+  } catch (err) {
+    // If the login request is invalid, return an error.
+    console.error(err);
+    return res.status(401).send("Unauthorized");
+  }
+
+  // Initialize the Firebase Admin SDK.
+  const { auth } = initializeFirebaseServer();
+
+  // Generate a JWT token for the user to be used on the client-side.
+  const token = await auth.createCustomToken(address);
+
+  // Send the token to the client to sign in with.
+  return res.status(200).json({ token });
+}
